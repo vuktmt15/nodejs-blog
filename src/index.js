8270d453dee663485dcbaae7a65714cda9ba19db
vuktmt15 <vuktmt15@gmail.com>
2021-12-16 16:49:34 +0700

    hbs commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.use(morgan('combined'))
+
+app.engine('hbs', handlebars({
+    extname: ".hbs"
+}))
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+
+app.get("/", (req, res) => {
+    res.render('home')
+})
+
+app.get("/news", (req, res) => {
+    res.render('news')
+})
+
+app.listen(port, () => {
+    console.log(`App run on http://localhost:${port}`)
+})
