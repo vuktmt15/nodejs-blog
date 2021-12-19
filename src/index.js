1e87046a0f9d8c4636a4350697706120a9685893
vuktmt15 <vuktmt15@gmail.com>
2021-12-19 10:25:18 +0700

    ...

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, '/public')))
+
 app.use(morgan('combined'))
 
 app.engine('hbs', handlebars({
