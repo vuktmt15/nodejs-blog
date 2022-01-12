bc6999d370fb0782d85163af411cb6358ca8bd76
vuktmt15 <vuktmt15@gmail.com>
2022-01-12 18:11:50 +0700

    update

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,17 +1,21 @@
 const Course = require('../models/Course.js')
-const {mutilpleMongooseToObject} = require('../../untils/mongoose')
+const {mutilpleMongooseToObject, mongooseToObject} = require('../../untils/mongoose')
 
 class CourseController {
     index(req, res, next) {
-        // Course.find({}, function (err, courses) {
-        //     if (!err) res.json(courses)
-        //     else res.status(400).json({error: 'Course not found'})
-        // })
         Course.find({})
             .then(courses => {
                 courses = mutilpleMongooseToObject(courses)
-                res.render('course', {courses})
-                //res.send(courses)
+                res.render('courses/index', {courses})
+            })
+            .catch(next)
+    }
+    show(req, res, next) {
+        const {slug} = req.params
+        Course.findOne({slug})
+            .then(course => {
+                course = mongooseToObject(course)
+                res.render('courses/show', {course})
             })
             .catch(next)
     }
--- src/routers/course.js
+++ src/routers/course.js
@@ -4,5 +4,6 @@ const CourseController = require('../app/controllers/CourseController')
 
 
 courseRoute.get('/', CourseController.index)
+courseRoute.get('/:slug', CourseController.show)
 
 module.exports = courseRoute
